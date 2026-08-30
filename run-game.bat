9f2d9250e27ba8cf21b7fefcd0a69a6f5fd5ffa8
Steve McCrea <stevemccrea@gmail.com>
2026-08-30 08:50:04 -0600

    Added run-game.bat - make.bat without the build

---
+++ run-game.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if exist "%~dp0setup-env.bat" call "%~dp0setup-env.bat"
+
+if defined VICE if exist "%VICE%" goto launch
+if defined VICE_BIN if exist "%VICE_BIN%\x64sc.exe" (
+  set VICE=%VICE_BIN%\x64sc.exe
+  goto launch
+)
+where x64sc >nul 2>&1 && (
+  for /f "delims=" %%i in ('where x64sc') do (
+    set VICE=%%i
+    goto launch
+  )
+)
+echo VICE x64sc not found. Install VICE or set VICE / VICE_BIN in setup-env.bat
+echo See SETUP.md
+exit /b 1
+
+:launch
+if not exist "%~dp0squaredoom.d64" (
+  echo squaredoom.d64 missing after build
+  exit /b 1
+)
+
+start "" "%VICE%" -silent -autostartprgmode 0 -8 "%~dp0squaredoom.d64" -autostart "%~dp0squaredoom.d64"
